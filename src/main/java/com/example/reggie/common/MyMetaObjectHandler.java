dafd89e52be4f0b6a3df80c7c4cab9e1b7b5d490
Minria <810760487@qq.com>
2022-04-30 23:17:17 +0800

    公共字段填充

---
+++ src/main/java/com/example/reggie/common/MyMetaObjectHandler.java
@@ -0,0 +1,30 @@
+package com.example.reggie.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+import java.time.LocalDateTime;
+
+@Slf4j
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    // 在插入数据时处理公共字段
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        metaObject.setValue("createTime", LocalDateTime.now());
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("createUser", BaseContext.getCurrentId());
+        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+        log.info(metaObject.toString());
+    }
+
+    //在修改时处理公共字段
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        metaObject.setValue("updateTime", LocalDateTime.now());
+        metaObject.setValue("createUser", BaseContext.getCurrentId());
+        metaObject.setValue("updateUser", BaseContext.getCurrentId());
+    }
+
+}
