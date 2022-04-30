9671048972c8681d8171f01fe4b589ac31a80b3b
Minria <810760487@qq.com>
2022-04-30 23:17:53 +0800

    订单详情控制

---
+++ src/main/java/com/example/reggie/controller/OrderDetailController.java
@@ -0,0 +1,10 @@
+package com.example.reggie.controller;
+
+import com.example.reggie.service.OrderDetailService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OrderDetailController {
+    @Autowired
+    private OrderDetailService orderDetailService;
+
+}
