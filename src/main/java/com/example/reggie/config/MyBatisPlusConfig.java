ec41a6b27b35786eb9dad4c9bc1a44117dc55d5a
Minria <810760487@qq.com>
2022-04-30 23:17:03 +0800

    java与数据库名字映射

---
+++ src/main/java/com/example/reggie/config/MyBatisPlusConfig.java
@@ -0,0 +1,17 @@
+package com.example.reggie.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyBatisPlusConfig {
+    // Java驼峰命名和数据库蛇形命名的映射设置
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor(){
+        MybatisPlusInterceptor mybatisPlusInterceptor=new MybatisPlusInterceptor();
+        mybatisPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
+        return mybatisPlusInterceptor;
+    }
+}
